843a49ad9bac56873a836dfb4a4e85fef11a1a49
rrsq123 <ranhua@live.cn>
2019-04-04 15:10:25 +0800

    合并分支后第1次commit

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -1,5 +1,6 @@
 public class test1 {
     public static void main(String[] args){
-        System.out.print("你好吗？");
+        System.out.println("你好吗？");
+        System.out.print("学习分支的合并与提交");
     }
 }
